d8be1b3fd98a805e3df9ab3c72442c581d9b6f7d
Raven-of-1849 <75646767+Raven-of-1849@users.noreply.github.com>
2023-11-05 19:07:11 -0600

    Refactor code, add user defined constants

--- main.c
+++ main.c
@@ -1,28 +1,30 @@
  #include <stdio.h>
  #include <math.h>
+ 
+ //User defined constants //
+ 
+ #define PAYRATE      12.00 // Employee hourly rate
+ #define WORKDAYS     7  //  Maximum workdays possible
+ #define TAXRATE_300  0.15 // First 300 tax rate
+ #define TAXRATE_150  0.20 // The next 150 tax rate
+ #define TAXRATE_REST 0.25 // Anything above the first two tax rates, 450 or more.
+
+ 
 
 int main() {
     
     float wageHours;                    // user entered wage hours, maximum is 168 hours per week//
     float overtimeWageHours;            // overtime hours, 
-    float basicPayRate;                 // Declaration only, value to be determined in another line
     float netWages;
     float taxDeductions;                // Total tax deductions
     int hoursInDay = 24;                // 24 hours in one day.
-    int workDays = 0;                   // number of work days, value to be determined in another line
     int overTimePay;
     
-    //*******  Maximum number of days possible in the work week  ***////
-    workDays = 7;
-    
-    //*****  Employee hourly wage   *****////
-    basicPayRate = 12;
-    
     //  Overtime wage formula  //
-    overTimePay = (1.5 * basicPayRate);
+    overTimePay = (1.5 * PAYRATE);
     
     //  Max labor hours possible  //
-    float maxPosibleHours = (workDays * hoursInDay);
+    float maxPosibleHours = (WORKDAYS * hoursInDay);
     
     
     //  Promp user for hours of labor  //
@@ -33,9 +35,6 @@ int main() {
     scanf("%f", &wageHours);
     
     
-     //  Gross wage for regular rate, (non-overtime)  //
-    float grossWages = (basicPayRate * wageHours);
-    
     //   Conditional statement to prevent labor hours from exceeding hours possible  // 
     
         if (wageHours > maxPosibleHours) {
@@ -44,101 +43,58 @@ int main() {
         
         } else {
             
+                    float grossWages;
+            
                     //  Conditional to check if the pay is overtime pay or regular pay  //
                     if (wageHours <= 40) {
                                   
+                                grossWages = (PAYRATE * wageHours); // Regular pay formula
+                                
                                 // Labor hours and gross pay  //
                                 printf("Regular labor hours: %.2f.\n Overtime hours: 0 \n ", wageHours);
                                 printf("Gross Pay: $%.2f\n", grossWages);
       
 
-                                //  Conditional to tax the first 300 dollars 15 percent  //
-                                if (grossWages <= 300) {
-                                    
-                                    taxDeductions = grossWages * 0.15; // Total tax deductions
-                                    netWages = grossWages - taxDeductions;
-                                    
-                                    printf("Tax deductions: $%.2f\n", taxDeductions);
-                                    printf("Net pay: $%.2f \n", netWages);
-                                    
-                                    return 0;
-                                    
-                                // Conditional to tax the additional 150 dollars 20 percent  //
-                                }  else if (grossWages> 300 && grossWages <= 450) { 
-                                    
-                                            taxDeductions = (300 * 0.15) + ( (grossWages - 300) * 0.20); // Total tax dexductions
-                                            netWages = grossWages - taxDeductions;
-                                            
-                                            printf("Tax deductions: $%.2f\n", taxDeductions);
-                                            printf("Net pay: $%.2f\n", netWages);
-                                            
-                                            return 0;
-                                    
-                                } else {
-                                        
-                                //  Remaining to be taxed 25 Percent  // 
-                                
-                                        taxDeductions = (300 * 0.15) + (150 * 0.20) + ( (grossWages - 450) * 0.25 ); // Total tax deductions
-                                        netWages = grossWages - taxDeductions;
-                                        
-                                        printf("Tax deductions: $%.2f \n", taxDeductions);
-                                        printf("Net pay: $%.2f\n", netWages);
-                                        
-                                        return 0;
-                                }    
-                      
-                        } else {
+                    } else {
                             
                                 /////*****   Overtime Conditional      *******///////
                             
-                            // Calculate overtime hours by subtracting 40 hours from hours worked
+                            // Calculate overtime hours by subtracting 40 hours from total hours worked
                             overtimeWageHours = (wageHours - 40);
                             
                             //Calculate total pay including overtime pay and basic pay * 40 hours
-                            grossWages = (overtimeWageHours * overTimePay) + (basicPayRate * 40);
+                            grossWages = (overtimeWageHours * overTimePay) + (PAYRATE * 40);
                             
                             
                             // Labor hours and gross pay //
                             printf("Regular labor hours: 40\n Overtime labor hours: %.2f \n", overtimeWageHours );
                             printf("Gross pay:  $%.2f\n", grossWages);
-                            
-//                            return 0;
 
-                                if (grossWages <= 300) {
+                            } 
+                            
+                            
+                            //*****   Tax conditional      ******//
+                            if (grossWages <= 300) {
      
-                                    taxDeductions = grossWages * 0.15; // Total tax deductions
-                                    netWages = grossWages - taxDeductions;
+                                    taxDeductions = grossWages * TAXRATE_300; // Total tax deductions
+//                                
+                            }  else if (grossWages> 300 && grossWages <= 450) { 
                                     
-                                    printf("Tax deductions: $%.2f\n", taxDeductions); 
-                                    printf("Net pay: $%.2f\n", netWages);
+                                    taxDeductions = (300 * 0.15) + ((grossWages - 300) * TAXRATE_150);  // Total tax deductions
                                     
-                                    return 0;
-                                    
-                                }  else if (grossWages> 300 && grossWages <= 450) { 
-                                    
-                                            taxDeductions = (300 * 0.15) + ((grossWages - 300) * 0.20);  // Total tax deductions
-                                            netWages = grossWages - taxDeductions;
-                                            
-                                            printf("Tax deductions: $%.2f\n", taxDeductions);
-                                            printf("Net pay $%.2f\n", netWages);
-                                            
-                                            return 0;
-                                    
-                                } else {
+                            } else {
                                         
-                                    
-                                        taxDeductions = (300 * 0.15) + (150 * 0.20) + ( (grossWages - 450) * 0.25 );  // Total tax deductions
-                                        netWages = grossWages - taxDeductions;
+                                    taxDeductions = (300 * 0.15) + (150 * 0.20) + ( (grossWages - 450) * TAXRATE_REST );  // Total tax deductions
                                         
-                                        
-                                        printf("Tax deductions: $%.2f\n", taxDeductions);
-                                        printf("Net pay:  $%.2f\n", netWages);
-                                        
-                                         return 0;
-                                }    
-                            }
-                            
+                                }  
 
+                        netWages = grossWages - taxDeductions; // Wage after taxes
+                        
+                        printf("Tax deductions: $%.2f\n", taxDeductions); // Print tax deductions //
+                        printf("Net pay:  $%.2f\n", netWages); // Print Net pay //
+                        
                         }
+                        
+                        return 0;
         
     }
